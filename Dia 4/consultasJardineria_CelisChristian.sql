b996a6deb443f409aed4665cebde4608597df543
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-05-07 14:00:55 -0500

    Consultas 07/05/2024

--- Dia 4/consultasJardineria_CelisChristian.sql
+++ Dia 4/consultasJardineria_CelisChristian.sql
@@ -131,20 +131,69 @@ where ciudad="Madrid" and codigoEmpleadoRepVentas = 11 or codigoEmpleadoRepVenta
 
 --ConsultaS Multitabla
 
+-- Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
 
--- 
-
-
-
-
-
+select c.nombreCliente as Cliente,  e.nombre as "Representante ventas", concat(e.apellido1," ",e.apellido2) as Apellidos
+from cliente as c
+inner Join empleado as e on c.codigoEmpleadoRepVentas = e.codigoEmpleado;
 
 
+-- Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+
+select c.nombreCliente, e.nombre as "Representante ventas"
+from cliente as c
+inner Join empleado as e on c.codigoEmpleadoRepVentas = e.codigoEmpleado
+where codigoCliente in(select codigoCliente from pago);
+
+
+-- Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+
+select c.nombreCliente, e.nombre as "Representante ventas", ofi.ciudad as "Ubicacion OFICINA"
+from cliente as c
+inner Join empleado as e on c.codigoEmpleadoRepVentas = e.codigoEmpleado
+inner Join oficina as ofi on ofi.codigoOficina = e.codigoOficina
+where codigoCliente in(select codigoCliente from pago) ;
+
+-- Devuelve el nombre de los clientes que  hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
+
+
+select c.nombreCliente, e.nombre as "Representante ventas",ofi.ciudad, ofi.lineaDireccion1
+from cliente as c
+inner Join empleado as e on c.codigoEmpleadoRepVentas = e.codigoEmpleado
+inner Join oficina as ofi on ofi.codigoOficina = e.codigoOficina
+where codigoCliente in(select codigoCliente from pago) and c.ciudad="Fuenlabrada";
+
+
+-- Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+
+select c.nombreCliente, e.nombre as "Representante ventas",ofi.ciudad
+from cliente as c
+inner Join empleado as e on c.codigoEmpleadoRepVentas = e.codigoEmpleado
+inner Join oficina as ofi on ofi.codigoOficina = e.codigoOficina;
 
 
 
+-- Devuelve un listado con el nombre de los empleados junto con el nombre de sus jefes.
 
 
+select e.nombre, e.codigoJefe, jf.nombre
+from empleado as e
+inner join empleado as jf on e.codigoJefe=jf.codigoEmpleado;
+
+--  Devuelve un listado que muestre el nombre de cada empleados, el nombre de su jefe y el nombre del jefe de sus jefe.
+
+select e.nombre as empleado,jf.nombre as jefe,jf2.nombre as jefedelJefe
+from empleado as e
+inner join empleado as jf on e.codigoJefe=jf.codigoEmpleado
+inner join empleado as jf2 on jf.codigoJefe=jf2.codigoEmpleado;
+
+-- Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
 
 
+select c.nombreCliente
+from cliente as c
+inner join pedido as p on p.codigoCliente=c.codigoCliente
+where p.fechaEntrega > p.fechaEsperada and p.estado="Entregado";
+
+-- Devuelve un listado de las diferentes gamas de producto que ha comprado cada cliente.
 
